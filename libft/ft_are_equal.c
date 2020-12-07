9d0cb4188ccb04d5cb485b2172890dcdb9d0650e
luca-kang <9from6@gmail.com>
2020-12-07 18:35:01 +0900

    add ft_are_equal.c

--- libft/ft_are_equal.c
+++ libft/ft_are_equal.c
@@ -5,6 +5,10 @@ bool	are_equal(char *s1, char *s2)
 	int			s1_len;
 	int			s2_len;
 
+	if (!s1 && !s2)
+		return (true);
+	if (!s1 || !s2)
+		exit(EXIT_FAILURE);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	if (s1_len == s2_len && ft_strncmp(s1, s2, ft_strlen(s1)) == 0)
