46f1e7e09055704d152957e15c53fa995b8e0381
luca-kang <9from6@gmail.com>
2020-12-16 18:32:17 +0900

    add test case for redirection

--- include/test.h
+++ include/test.h
@@ -6,6 +6,7 @@
 char 	*get_err_msg(char *func_name, int case_number, char *expect, char *result);
 void	test_get_command_line(t_list *env);
 void	test_command_lines(t_list *env);
-
+void	test_validate_line();
+void	test_redirection(t_list *env);
 
 #endif
