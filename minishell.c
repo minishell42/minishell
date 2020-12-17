ab702c0047b00e492b3bce0814110ec21b0ad6f2
luca-kang <9from6@gmail.com>
2020-12-18 01:05:36 +0900

    add signal handler function

--- minishell.c
+++ minishell.c
@@ -7,6 +7,8 @@ void	minishell(char *line, t_list *env)
 
 	g_err.err_number = 0;
 	g_err.err_value = 0;
+	signal(SIGINT, child_exit);
+	signal(SIGQUIT, child_exit);
 	if (!validate_line(line))
 	{
 		printf("errnum = %d\n", g_err.err_number);
@@ -23,11 +25,16 @@ void	minishell(char *line, t_list *env)
 				print_err_msg();
 			return ;
 		}
+		// if (are_equal(command_line->command, "a"))
+		// {
+		// 	while (42)
+		// 		printf("err\n");
+		// }
 		res = run_command(command_line, env);
 		redirection(command_line, env, res); // bool type
 		free(res);
-
-		
+		free_cmd_struct(command_line);
+		free(command_line);
 		// run_redirection(res, command_line, env);
 		// if (g_err.err_number)
 			// print_err_msg();
