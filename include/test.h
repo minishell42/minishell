3905c1125f82928ad312ef35841e21773f1519f8
ParkSanggi <roqkfwkehlwk@naver.com>
2020-12-18 04:35:06 +0900

    add test case for export

--- include/test.h
+++ include/test.h
@@ -8,5 +8,6 @@ void	test_get_command_line(t_list *env);
 void	test_command_lines(t_list *env);
 void	test_validate_line();
 void	test_redirection(t_list *env);
+void	test_export(t_list *env);
 
 #endif
