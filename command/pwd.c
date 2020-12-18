eee2d688feb302f542a728bb5098188b357e349c
luca-kang <9from6@gmail.com>
2020-12-18 11:24:23 +0900

    fix pwd function

--- command/pwd.c
+++ command/pwd.c
@@ -10,12 +10,6 @@ char	*pwd(t_cmd_line *cmd_line, char **pipe_flag)
 	dir = ft_strjoin(tmp, "\n");
 	if (!cmd_line->redir_flag && !cmd_line->pipe_flag)
 		write(1, dir, ft_strlen(dir));
-	if (cmd_line->pipe_flag)
-		if (*pipe_flag)
-		{
-			free(*pipe_flag);
-			*pipe_flag = NULL;
-		}
 	free(tmp);
 	return (dir);
 }
