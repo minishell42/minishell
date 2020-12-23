b3950147ef7b0ccba9b2760b02e57466a18e1995
ParkSanggi <roqkfwkehlwk@naver.com>
2020-12-23 17:32:16 +0900

    add pipes struct

--- include/minishell.h
+++ include/minishell.h
@@ -3,6 +3,13 @@
 
 # include "command.h"
 
+
+typedef struct	s_pipe_fd
+{
+	int		old[2];
+	int		new[2];
+}				t_pipes;
+
 void				excute(t_cmd_line *cmd_line, char *envp[]);
 t_list 				*get_env_llist(char *envp[]);
 /*
