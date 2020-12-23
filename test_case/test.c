e779b9b810dead0f573803f0cc28055188090bf3
luca-kang <9from6@gmail.com>
2020-12-23 10:29:42 +0900

    test pipe and dup function

--- test_case/test.c
+++ test_case/test.c
@@ -1,76 +1,70 @@
 #include "test.h"
 
+void	divide_process(int *p_fd, bool flag, int i)
+{
+	int pid2;
+	int status;
+	char buff[BUFFER_SIZE];
+
+	pid2 = fork();
+	ft_bzero(buff, BUFFER_SIZE);
+	if (pid2 > 0)
+	{
+		printf("2nd\tprocess ? %d\tpid ? %d\n",getpid(), pid2);
+		waitpid(pid2, &status, 0);
+		if (i == 2)
+		{
+			read(p_fd[0], buff, BUFFER_SIZE);
+			printf("parents pipe buffer ? %s\n", buff);
+		}
+	}
+	else if (pid2 == 0)
+	{
+		printf("3th\tprocess ? %d\tpid ? %d\n",getpid(), pid2);
+		dup2(p_fd[1], 1);
+		printf("sample\tindex ? %d\n", i);
+		if (flag)
+		{
+			dup2(p_fd[0], 0);
+			printf("ex\n");
+			// read(0, buff, BUFFER_SIZE);
+			// printf("child pipe buffer ? %s\tindex ? %d\n", buff, i);
+		}
+		exit(0);
+	}
+}
+
 int main(int argc, char **argv, char *envp[])
 {
-	// t_list *env;
-
 	if (argc != 1)
 		return (0);
 	argv[1] = 0;
-	// env = get_env_llist(envp);
-	// test_get_command_line(env);
-	// test_cd(env);
-	//  test_command_lines(env);
-	// test_redirection(env);
-	// test_validate_line();
 
-	// test_export(env);
-	// ft_lstclear(&env, free);
-
-	int p[2];
+	int p_fd[2];
 	int pid;
-	int pid2;
 	int status;
-	const char *path = 	"/bin/echo";
-	char **values = ft_calloc(sizeof(char *), 5);
-	char buf[255];
+	int	i;
+	bool flag;
 
-	ft_bzero(buf, 255); // grep 1 < 456
-	values[0] = "echo";
-	values[1] = "1";
-	values[2] = "2";
-	values[3] = "456";
-	// values[1] = "\0";
-	// values[2] = "";
-
-	pipe(p);
-	pid = fork();  // p c1
+	pipe(p_fd);
+	pid = fork();
+	i = 0;
+	flag = false;
 	if (pid == 0)
 	{
-		printf("start child\n");
-		
-		pid2 = fork(); // c1 c2
-		if (pid2 > 0)
+		while (i < 3)
 		{
-			waitpid(pid2, &status, 0); // c1 wait -> pipe 1 : 123456
-			printf("status : %d\n", status);
-			printf("end child22222\n");
-			// close(p[1]);
-			read(p[0], buf, 255); // buf -> 123456
-			write(1, buf, 255); // 1 -> 123456
-			// printf("%s\n", buf);
-			// dup2(1, sout);
-			printf("recover pipe\n");
-			exit(1);
+			divide_process(p_fd, flag, i);
+			close(p_fd[1]);
+			flag = true;
+			i++;
 		}
-		else if (pid2 == 0) // c2 -> 123456
-		{
-			printf("start child2222\n");
-			dup2(p[1], 1);
-			// write(1, "", 1);
-			// exit(5);
-			// printf("write pipe = %d\n", p[1]);
-			if (execve(path, values, envp) == -1)
-				perror("excve\n");
-		}
-
 	}
 	else if (pid > 0)
 	{
-		waitpid(pid, &status, 0); // p wait ->  1 : 123456
-		printf("end child\n");
+		printf("1st\tprocess ? %d\tpid ? %d\n",getpid(), pid);
+		waitpid(pid, &status, 0);
 	}
-	free(values);
 	return (0);
 }
 
