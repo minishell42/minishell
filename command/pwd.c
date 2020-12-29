f88303f6bada1031bd496935c3144b473fb1da37
luca-kang <9from6@gmail.com>
2020-12-29 16:55:37 +0900

    debug pwd function which exist memory leaks

--- command/pwd.c
+++ command/pwd.c
@@ -8,9 +8,9 @@ bool	pwd(t_cmd_line *cmd_line)
 	if (!(tmp = getcwd(NULL, 0)))
 		return (false);
 	dir = ft_strjoin(tmp, "\n");
+	free(tmp);
 	if (!(write(1, dir, ft_strlen(dir))))
 		return (false);
-	free(tmp);
 	free(dir);
 	return (true);
 }
