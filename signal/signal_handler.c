8dc700bede241fc6df2900c601565a06174167c2
luca-kang <9from6@gmail.com>
2020-12-30 18:00:57 +0900

    add init_signal and init_child_signal function

--- signal/signal_handler.c
+++ signal/signal_handler.c
@@ -9,18 +9,7 @@ void		prompt(void)
 	write(1, str, ft_strlen(str));
 }
 
-//----------------- before --------------------
-// void		handler_signal(int signal_no)
-// {
-// 	if (signal_no == SIGINT)
-// 		ft_putstr_fd("\b\b\b\b\n", 1);
-// 	else if (signal_no == SIGQUIT)
-// 		ft_putstr_fd("\b\b\b\b\n", 1);
-// }
-
-//----------------- after --------------------
-// add prompt function
-void		handler_signal(int signal_no)
+void		default_signal(int signal_no)
 {
 	if (signal_no == SIGINT)
 	{
@@ -34,6 +23,32 @@ void		handler_signal(int signal_no)
 	}
 }
 
+void		child_signal(int signal_no)
+{
+	if (signal_no == SIGINT)
+	{
+		ft_putchar_fd('\n', 1);
+		exit(signal_no);
+	}
+	else if (signal_no == SIGQUIT)
+	{
+		ft_putstr_fd("Quit: 3\n", 2);
+		exit(signal_no);
+	}
+}
+
+void		init_signal(void)
+{
+	signal(SIGINT, default_signal);
+	signal(SIGQUIT, default_signal);
+}
+
+void		init_child_signal(void)
+{
+	signal(SIGINT, child_signal);
+	signal(SIGQUIT, child_signal);
+}
+
 void		process_exit(bool option)
 {
 	char	*str;
@@ -43,38 +58,4 @@ void		process_exit(bool option)
 	str = B_YELLOW"minishell exit\n";
 	write(1, str, ft_strlen(str));
 	exit(0);
-}
-
-void		child_exit(int signo)
-{
-	char	*str;
-
-	str = B_YELLOW"실행중인 프로그램을 중지합니다.\n";
-	ft_putstr_fd("\b\b\b\b\n", 1);
-	write(1, str, ft_strlen(str));
-	exit(signo);
-}
-
-void		set_signal(void)
-{
-	pid_t	pid;
-	int		status;
-	
-	pid = fork();
-	if (pid > 0)
-	{
-		signal(SIGINT, handler_signal);
-		signal(SIGQUIT, handler_signal);
-	}
-	while (waitpid(pid, &status, 0) > 0)
-	{
-		if (!status)
-			exit(0);
-		pid = fork();
-	}
-	if (pid == 0)
-	{
-		signal(SIGINT, exit);
-		signal(SIGQUIT, exit);
-	}
 }
