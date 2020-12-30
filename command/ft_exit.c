2b6d87b9b6c598c5c5f7fbde060b877570b901d9
luca-kang <9from6@gmail.com>
2020-12-30 17:49:29 +0900

    refact ft_exit function

--- command/ft_exit.c
+++ command/ft_exit.c
@@ -1,10 +1,8 @@
-// #include "command.h"
+#include "command.h"
 
-// char	*ft_exit(t_cmd_line *cmd_line, t_list *env, char **pipe_input)
-// {
-// 	free_cmd_struct(cmd_line);
-// 	free(*pipe_input);
-// 	free_env_list(&env);
-// 	free(cmd_line);
-// 	process_exit(EXIT);
-// }
+char	*ft_exit(t_cmd_line *cmd_line)
+{
+	free_cmd_struct(cmd_line);
+	free_env_list(&g_env);
+	process_exit(0);
+}
